da18561d0990138b99c606a7f88d95051502bc81
Wladimir Mendoza <wjms200@gmail.com>
2022-11-18 21:45:25 -0500

    hola perros

--- dart/pokemon/bin/pokemon.dart
+++ dart/pokemon/bin/pokemon.dart
@@ -3,4 +3,6 @@ import 'package:pokemon/pokemon.dart' as pokemon;
 void main(List<String> arguments) {
   print('Hello world: ${pokemon.calculate()}!');
   // Commentarios
+
+  print('hola perros');
 }
