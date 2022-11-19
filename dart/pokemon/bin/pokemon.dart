187bbeb31cb3fb9c7b1f1718d472383f2c1d82a7
Vicentemr2002 <80923963+Vicentemr2002@users.noreply.github.com>
2022-11-19 00:15:46 -0300

    La veldad

--- dart/pokemon/bin/pokemon.dart
+++ dart/pokemon/bin/pokemon.dart
@@ -4,23 +4,6 @@ void main(List<String> arguments) {
   print('Hello world: ${pokemon.calculate()}!');
   // Commentarios
 
-  print('hola perros');
-  print("guachasos");
-  print('actualizacion de commit');
-<<<<<<< Updated upstream
-  print("jose no llego lo tuyo");
-  print("Supp roba kills");
-  print("El lol es una basura");
-  print("N");
-<<<<<<< HEAD
-  print("El lol es una basura")
+print("El jungla y el supp son robas kills");
 
-=======
-=======
-=======
->>>>>>> Stashed changes
-  print("prueba");
-  print("Hola mi hermano");
->>>>>>> master
->>>>>>> master
 }
