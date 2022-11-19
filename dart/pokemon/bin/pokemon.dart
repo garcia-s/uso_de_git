20e1bd1fc36f79f5e00ac92d761f33d53b5557fa
Juan Guzman <juancguzman16@gmail.com>
2022-11-19 00:09:45 -0300

    Auto stash before merge of "master" and "origin/master"

--- dart/pokemon/bin/pokemon.dart
+++ dart/pokemon/bin/pokemon.dart
@@ -7,12 +7,15 @@ void main(List<String> arguments) {
   print('hola perros');
   print("guachasos");
   print('actualizacion de commit');
+<<<<<<< Updated upstream
   print("jose no llego lo tuyo");
 <<<<<<< HEAD
   print("Supp roba kills");
   print("El lol es una basura");
   print("N");
 =======
+=======
+>>>>>>> Stashed changes
   print("prueba");
   print("Hola mi hermano");
 >>>>>>> master
