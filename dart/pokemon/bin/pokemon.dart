4935e79a4e066fa37d4c6fe5344e152b1bf25632
Juan Garcia <garciajj18@gmail.com>
2022-11-18 22:14:50 -0500

    Enterate Ramon

--- dart/pokemon/bin/pokemon.dart
+++ dart/pokemon/bin/pokemon.dart
@@ -3,20 +3,6 @@ import 'package:pokemon/pokemon.dart' as pokemon;
 void main(List<String> arguments) {
   print('Hello world: ${pokemon.calculate()}!');
   // Commentarios
-<<<<<<< HEAD
-print('nerf akaly');
-=======
 
-  print('hola perros');
-  print("guachasos");
-  print('actualizacion de commit');
-  print("jose no llego lo tuyo");
-  print("Supp roba kills");
-  print("El lol es una basura");
-  print("N");
-  print("prueba");
-  print("Hola mi hermano");
-=======
->>>>>>> Stashed changes
->>>>>>> master
+  print('nerf akaly');
 }
